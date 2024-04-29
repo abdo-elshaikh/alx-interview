3ef7f70c729f22780ea822891253c91a0261101d
abdo-elshaikh <abdo_mhmd1@hotmail.com>
2024-04-29 20:53:35 +0300

    complete lock boxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def canUnlockAll(boxes):
+    """
+    canUnlockAll
+    """
+    keys = [0]
+    n = len(boxes)
+    for i in range(0, n - 1):
+        for j in boxes[i]:
+            if j not in keys:
+                keys.append(j)
+    for i in range(0, n):
+        if i not in keys:
+            return False
+    return True
